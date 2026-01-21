5f2b5e2c58f034d683bfa8f4bb2ab1480a67ad63
Priscila Santos <psds1@aluno.ifnmg.edu.br>
2026-01-21 20:27:55 -0300

    Integracao

---
+++ src/pages/integra.php
@@ -0,0 +1,42 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Menu</title>
+
+    <link rel="stylesheet" href="../styles/global.css">
+    <link rel="stylesheet" href="../styles/index.css">
+
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/regular/style.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/fill/style.css"
+    />
+</head>
+
+<body>
+
+<header class="header">
+    <h1 class="title">Menu</h1>
+</header>
+
+<main class="main">
+
+    <div class="menu-buttons">
+        <a href="relatorios.php" class="button">Relatórios</a>
+        <a href="controle_comissarios.php" class="button">Controle de Comissionários</a>
+    </div>
+
+</main>
+
+<script src="../scripts/index.js"></script>
+</body>
+</html>
+
